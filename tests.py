48c18d00d528ef1fe14836b839534f454f5aa88d
Vadim <popperony@yandex.ru>
2020-09-24 08:45:33 +0300

    4.1.1

--- tests.py
+++ tests.py
@@ -9,7 +9,10 @@ class FlaskAppTests(unittest.TestCase):
 
     def test_get(self):
         r = self.app.get('/')
-        self.assertEqual(r.data, b'\xd0\xa3 \xd0\xbc\xd0\xb5\xd0\xbd\xd1\x[80 chars]x8c!')
+        self.assertEqual(
+            r.data,
+            b'\xd0\xa3 \xd0\xbc\xd0\xb5\xd0\xbd\xd1\x[80 chars]x8c!'
+        )
 
     def test_post(self):
         r = self.app.post('/')
